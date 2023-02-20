8bc7b5bfcd2b1bc1ba78e74d1c37a3b88a295a44
Weirdchupacabra <107323289+Weirdchupacabra@users.noreply.github.com>
2023-02-20 17:42:05 +0100

    Update Kalkulator Pentagonu Kurwa Genialny.py

--- Kalkulator Pentagonu Kurwa Genialny.py
+++ Kalkulator Pentagonu Kurwa Genialny.py
@@ -29,7 +29,7 @@ while True:
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2)
         elif choice == '3':
-            print(num1, "*", num2, "=', multiply(num1, num2)
+            print(num1, "*", num2, "=", multiply(num1, num2)
         else:
             print(num1, "/", num2, "=", divide(num1, num2))
         
